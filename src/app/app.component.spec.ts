fb56547dba0d57485014c8a4a8ceb44a76142010
Shiv Kushwaha <shiv@wizni.com>
2017-01-08 14:50:35 +0530

    + test case refactoring

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,9 @@
+import * as chai from 'chai';
+const expect = chai.expect;
+
+
+describe('ProductComponent', () => {
+  it(`should show product 'Mobile'`, () => {
+    console.log('data');
+  });
+});
