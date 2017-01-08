f80e7f75a48523989dcb0b3e01167da4b5819cbe
Shiv Kushwaha <shiv@wizni.com>
2017-01-08 15:39:46 +0530

    + merging of css and html file to component metadata ++ test case refactoring

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,41 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
 import * as chai from 'chai';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 const expect = chai.expect;
 
+describe(`App`, () => {
+  let comp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-describe('ProductComponent', () => {
-  it(`should show product 'Mobile'`, () => {
-    console.log('data');
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [],
+      imports: [BrowserModule,
+        FormsModule,
+        HttpModule]
+    })
+      .compileComponents(); // compile template and css
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+    fixture.detectChanges(); // trigger initial data binding
+  });
+
+  it(`should be readly initialized`, () => {
+    expect(!!fixture).to.be.true;
+    expect(!!comp).to.be.true;
+  });
+
+  it(`should title be Continuous Integration with Travis-CI`, () => {
+    expect(comp.title).to.equal('Continuous Integration with Travis-CI');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,53 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['app.component.css'],
-  encapsulation: ViewEncapsulation.None,
+  template: `<div>
+  <div>
+    <h2>Calculator!</h2>
+    <div>
+      <label>A: </label>
+      <input id="num1" [(ngModel)]="num1" placeholder="Enter a number"/>
+    </div>
+    <div>
+      <label>B: </label>
+      <input id="num2" [(ngModel)]="num2" placeholder="Enter a number"/>
+    </div>
+    <button id="save" (click)="save()" style="margin-left: 30px;width: 200px; height: 35px;">Add</button>
+  </div>
+  <hr>
+  <div>
+    <label id="sum">Sum:{{sum}}</label>
+  </div>
+</div>
+`,
+  styles: [`label {
+  display: inline-block;
+  width: 3em;
+  margin: .5em 0;
+  color: #607D8B;
+  font-weight: bold;
+}
+input {
+  height: 2em;
+  font-size: 1em;
+  padding-left: .4em;
+}
+button {
+  margin-top: 20px;
+  font-family: Arial;
+  background-color: #eee;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 4px;
+  cursor: pointer; cursor: hand;
+}
+button:hover {
+  background-color: #cfd8dc;
+}`]
 })
 export class AppComponent {
+  public title = 'Continuous Integration with Travis-CI';
   private num1: number;
   private num2: number;
   private sum : number= 0;
